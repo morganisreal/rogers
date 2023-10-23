00f72b344a95eba9920f063e3d08c6390d85d164
morganisreal <148760038+morganisreal@users.noreply.github.com>
2023-10-23 02:27:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_lbtugne", "template_3f2zqfg", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
